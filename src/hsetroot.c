c8862512940058c486c84d7f58dd8c111795ac5d
Pete Beardmore <pete.beardmore@msn.com>
2018-02-03 18:39:02 +0000

    licencing info
    -mark source files
    -add standard LICENCE and AUTHORS files

--- src/hsetroot.c
+++ src/hsetroot.c
@@ -1,3 +1,29 @@
+//////////////////////////////////////////////////////////////////////
+/*
+
+  Copyright (C) 2004-2012 Hyriand
+                2012 Alessandro Ghedini
+                2015 Martin Hradil
+                2018 Pete Beardmore
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+  http://www.gnu.org/licenses/gpl-2.0.txt
+
+*/
+//////////////////////////////////////////////////////////////////////
+
 #include <X11/Xlib.h>
 #include <X11/Xatom.h>
 #include <Imlib2.h>
